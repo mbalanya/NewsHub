8393501074c2b99d12e362063424521cf16a27a6
mbalanya <okobacharles@yahoo.co.uk>
2020-08-19 00:00:22 +0300

    add toast to in MainActivity

--- app/src/main/java/com/chalookoba/newshub/MainActivity.java
+++ app/src/main/java/com/chalookoba/newshub/MainActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button mTrendingNewsButton;
@@ -18,7 +19,7 @@ public class MainActivity extends AppCompatActivity {
         mTrendingNewsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                Toast.makeText(MainActivity.this, "Hot News", Toast.LENGTH_LONG).show();
             }
         });
     }
