954f44cea21b05b5b9d9a3b04cb673aa020aeb72
mbalanya <okobacharles@yahoo.co.uk>
2020-08-19 02:58:20 +0300

    add listview to activity_trending_news

--- app/src/main/java/com/chalookoba/newshub/TrendingNewsActivity.java
+++ app/src/main/java/com/chalookoba/newshub/TrendingNewsActivity.java
@@ -4,12 +4,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class TrendingNewsActivity extends AppCompatActivity {
     private TextView mHeadlineTextView;
+    private ListView mListview;
+    private String[] restaurants = new String[] {"Mi Mero Mole", "Mother's Bistro",
+            "Life of Pie", "Screen Door", "Luc Lac", "Sweet Basil",
+            "Slappy Cakes", "Equinox", "Miss Delta's", "Andina",
+            "Lardo", "Portland City Grill", "Fat Head's Brewery",
+            "Chipotle", "Subway"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
