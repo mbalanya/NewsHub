16287a86ce6199ba254e9c6f2c31b95c3a6eb01f
mbalanya <okobacharles@yahoo.co.uk>
2020-08-19 00:07:52 +0300

    add TrendingNewsActivity

---
+++ app/src/main/java/com/chalookoba/newshub/TrendingNewsActivity.java
@@ -0,0 +1,14 @@
+package com.chalookoba.newshub;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TrendingNewsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_trending_news);
+    }
+}
