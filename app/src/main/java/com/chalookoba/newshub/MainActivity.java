e466f4b81d89c56ec0cbf5a22cb1dbc595418cec
mbalanya <okobacharles@yahoo.co.uk>
2020-08-18 23:57:14 +0300

    import button in MainActivity

--- app/src/main/java/com/chalookoba/newshub/MainActivity.java
+++ app/src/main/java/com/chalookoba/newshub/MainActivity.java
@@ -3,12 +3,23 @@ package com.chalookoba.newshub;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+    private Button mTrendingNewsButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mTrendingNewsButton = (Button)findViewById(R.id.trendingNewsButton);
+        mTrendingNewsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
