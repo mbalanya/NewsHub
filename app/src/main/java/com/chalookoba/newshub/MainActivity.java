3154782ea4e373a630d9ba2a18eb56a70970b3ed
mbalanya <okobacharles@yahoo.co.uk>
2020-08-19 00:27:58 +0300

    add Intent to TrendingNewsActivity

--- app/src/main/java/com/chalookoba/newshub/MainActivity.java
+++ app/src/main/java/com/chalookoba/newshub/MainActivity.java
@@ -2,24 +2,34 @@ package com.chalookoba.newshub;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String TAG = MainActivity.class.getSimpleName();
     private Button mTrendingNewsButton;
+    private EditText mHeadlineEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mHeadlineEditText = (EditText)findViewById(R.id.headlineEditText);
         mTrendingNewsButton = (Button)findViewById(R.id.trendingNewsButton);
         mTrendingNewsButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String headline = mHeadlineEditText.getText().toString();
+                Log.d(TAG, headline);
                 Toast.makeText(MainActivity.this, "Hot News", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, TrendingNewsActivity.class);
+                startActivity(intent);
             }
         });
     }
