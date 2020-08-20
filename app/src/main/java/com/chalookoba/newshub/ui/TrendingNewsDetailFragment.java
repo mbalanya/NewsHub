9866775b1b12642960b8e8fe84b1ae4da5b6d3e8
mbalanya <okobacharles@yahoo.co.uk>
2020-08-21 00:18:23 +0300

    link data to TrendingNewsDetailFragment

--- app/src/main/java/com/chalookoba/newshub/ui/TrendingNewsDetailFragment.java
+++ app/src/main/java/com/chalookoba/newshub/ui/TrendingNewsDetailFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.os.Parcel;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,8 +13,16 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.chalookoba.newshub.R;
+import com.chalookoba.newshub.models.Article;
+import com.squareup.picasso.Picasso;
+
+import org.parceler.Parcels;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -28,6 +37,8 @@ public class TrendingNewsDetailFragment extends Fragment {
     @BindView(R.id.contentTextView) TextView mContentLabel;
     @BindView(R.id.linkTextView) TextView mLinkLabel;
     @BindView(R.id.saveTrendingNewsDetailButton) Button mSaveTrendingNewsDetailButton;
+
+    private Article mArticle;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -46,33 +57,40 @@ public class TrendingNewsDetailFragment extends Fragment {
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
+     * @param article Parameter 1.
+     * @param article Parameter 2.
      * @return A new instance of fragment TrendingNewsDetailFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static TrendingNewsDetailFragment newInstance(String param1, String param2) {
-        TrendingNewsDetailFragment fragment = new TrendingNewsDetailFragment();
+    public static TrendingNewsDetailFragment newInstance(Article article) {
+        TrendingNewsDetailFragment trendingNewsDetailFragment = new TrendingNewsDetailFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
+        args.putParcelable("article", Parcels.wrap(article));
+        trendingNewsDetailFragment.setArguments(args);
+        return trendingNewsDetailFragment;
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+        mArticle = Parcels.unwrap(getArguments().getParcelable("article"));
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trending_news_detail, container, false);
+        View view = inflater.inflate(R.layout.fragment_trending_news_detail, container, false);
+        ButterKnife.bind(this, view);
+
+        Picasso.get().load(mArticle.getUrlToImage()).into(mImageLabel);
+
+        mTitleLabel.setText(mArticle.getTitle());
+        mAuthorLabel.setText(mArticle.getAuthor());
+        mPublishedLabel.setText(mArticle.getPublishedAt());
+        mContentLabel.setText(mArticle.getContent());
+        mLinkLabel.setText(mArticle.getUrl());
+
+        return view;
     }
 }
